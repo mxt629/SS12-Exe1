eccc6370d9c595ab26a3bb09ce25e66287172ebe
mxt629 <xuantien.06021990@gmail.com>
2022-03-27 10:22:04 +0700

    Initial Commit

---
+++ SS12 Exe1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS12 Exe1
+//
+//  Created by Little Chick on 27/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
